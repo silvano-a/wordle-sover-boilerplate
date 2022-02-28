dd229fa843eb5b6b857b67d5d8d61be32c0c3316
benno6194 <benno6194@gmail.com>
2022-02-28 09:05:53 +0100

    add functions to gamesolver

--- app/Wordle/WordleGameSolver.php
+++ app/Wordle/WordleGameSolver.php
@@ -6,13 +6,26 @@ class WordleGameSolver
 {
     /** @var Game */
     private $game;
+    private $wordList;
+    private $guesses = [];
 
     public function start(Game $game)
     {
         $this->game = $game;
+        $this->wordList = $game->wordlist;
 
-        dd(
-            $this->game->guessRow('whack') // make guess that the word is 'whack'. returns validation Row::class object.
-        );
+        $this->guesses[] = $this->game->guessRow('valid'); // make guess that the word is 'whack'. returns validation Row::class object.
+    }
+
+    public function analyse()
+    {
+        //foreach guesses filter woordlijst
+    }
+
+    public function guess()
+    {
+        //this game guessRow()
+        $this->analyse();
+        $this->guesses[] = $this->game->guessRow(reset($this->wordList));
     }
 }
